cd4883f99f5ac24f75d4f0ca7a5e4614124556f4
tharunyadav6 <126887969+tharunyadav6@users.noreply.github.com>
2025-03-14 13:56:26 +0530

    Fixed incorrect Redux import path

--- src/Components/WishtleCom.jsx
+++ src/Components/WishtleCom.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 // import { removeFromWishList } from '../Redux/Slices/wishListSlice.js';
-import { removeFromWishList } from "@Redux/Slices/wishListSlice.js"
+import { removeFromWishList } from "../Redux/Slices/wishListSlice.js"
 
 // import { removeFromWishList } from "../Redux/Slices/wishListSlice";
 
